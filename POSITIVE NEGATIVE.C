ae232e5336b2d65e09298acd7cb75f536fda4abf
yamunasweety <35589149+yamunasweety@users.noreply.github.com>
2018-01-19 11:50:19 +0530

    POSITIVE NEGATIVE.C
    PROGAM TO FIND  THE NUMBER IS POSITIVE OR NEGATIVE OR ZERO

---
+++ POSITIVE NEGATIVE.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter any number:");
+scanf("%d",&num);
+if(num>0)
+{
+printf("number is positive");
+}
+if(num<0)
+{
+printf("number is negative");
+}
+if(num==0)
+{
+printf("number is zero");
+}
+return 0;}
+
