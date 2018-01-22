ccf2abae723bdf3ea0eb4df58bd8df2c1a3b61f8
yamunasweety <35589149+yamunasweety@users.noreply.github.com>
2018-01-22 16:50:55 +0530

    ARRAY.C
    program to find the sum of first integer numbers

---
+++ ARRAY.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int i,j,k,l;
+printf("enter the number:");
+scanf("%d",&i);
+for(k=0;k<=i;k++)
+{
+printf("%d\n",i);
+}
+for(j=0;j<=i;j++)
+{
+l+=j;
+}
+getch();
+}
