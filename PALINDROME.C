dba967d5e926311531364ec95f4f625748ef6263
yamunasweety <35589149+yamunasweety@users.noreply.github.com>
2018-01-31 16:14:33 +0530

    PALINDROME.C
    TO FIND THE NUMBER IS PALINDROME OR NOT

---
+++ PALINDROME.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n,reversed integer=0;remainder,original integer;
+printf("enter an integer:");
+scanf("%d",&n);
+original integer=n;
+while(n!=0)
+}
+remainder=n%10;
+reversed integer=reversed integer*10+remainder;
+n/=10;
+}
+if(original integer==reversed integer)
+printf("%d is a palindrome.",original integer);
+rlse
+printf("%d is not a palindrome.",original integer);
+return 0;
+}
