f8abb938e8364056790158afbc45a073f16d7fc0
yamunasweety <35589149+yamunasweety@users.noreply.github.com>
2018-02-09 16:25:33 +0530

    NUMERIC.C
    to find the numeric numbers

---
+++ NUMERIC.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char text[80], ch ;
+    int numeric character=0;
+    clrscr() ;
+    printf("Enter the text : ") ;
+    gets(text) ;
+    while((ch = tolower(text[i++])) != '\0')
+    {
+       word ;
+    printf("\nThe text contains : ") ;
+    printf("\n\nNumber of numeric character:");
+    return 0;
+    }
