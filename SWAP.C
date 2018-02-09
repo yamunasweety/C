4410f42b3e29832bb86505d7be97861cd250aec6
yamunasweety <35589149+yamunasweety@users.noreply.github.com>
2018-02-09 16:41:19 +0530

    SWAP.C
    TO FIND SWAP FUNCTION

---
+++ SWAP.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    double firstNumber, secondNumber;
+    printf("Enter first number: ");
+    scanf("%lf", &firstNumber);
+    printf("Enter second number: ");
+    scanf("%lf",&secondNumber)
+    firstNumber = firstNumber - secondNumber;
+    secondNumber = firstNumber + secondNumber;
+    firstNumber = secondNumber - firstNumber;
+    printf("After swapping, firstNumber = %.2lf\n", firstNumber);
+    printf("After swapping, secondNumber = %.2lf", secondNumber);
+    return 0;
+}
