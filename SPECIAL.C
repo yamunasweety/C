b60df14fdcf29b72496e60ff7e8c661b77465190
yamunasweety <35589149+yamunasweety@users.noreply.github.com>
2018-02-09 16:36:21 +0530

    SPECIAL.C
    TO FIND THE SPECIAL CHARACTERS

---
+++ SPECIAL.C
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+int main()
+{
+    char string[MAX_SIZE];
+    int special characters;
+    printf("\nEnter any string : ");
+    gets(string);
+    while(string[i]!='\0')
+    {
+        if((string[i]>='a' && string[i]<special characters);
+        {
+          special characters++;
+    }
+    printf("\nSpecial characters = [ %d ]\n", others);
+    return 0;
+}
