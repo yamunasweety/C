ee3dbda75e074874db168f35e88066d627597dee
yamunasweety <35589149+yamunasweety@users.noreply.github.com>
2018-02-19 15:56:47 +0530

    RANGE.C
    TO FIND THE RANGE

---
+++ RANGE.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("enter the value:\n");
+	scanf("%d",&n);
+	if(n>=1&&n<=10)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
