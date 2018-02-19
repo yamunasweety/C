c743f9f42d15e018fdcc80b636070b3e1b819fda
yamunasweety <35589149+yamunasweety@users.noreply.github.com>
2018-02-19 16:40:05 +0530

    SUUUM.C
    TO FIND THE SUM OF NUMBERS

---
+++ SUUUM.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a=15,b=10,s;
+    printf("enter the number a:\n");
+    printf("enter the number b:\n");
+    scanf("%d%d",&a,&b);
+    s=a+b;
+    printf("total is :%d",s);
+    return 0;
+}
