f73abdcbb68581f04f2cd21a2194b7efedbb4391
yamunasweety <35589149+yamunasweety@users.noreply.github.com>
2018-02-23 15:49:27 +0530

    NEAREST.C
    TO FIND THE NEAREST EVEN NUMBERS

---
+++ NEAREST.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() 
+{
+int s,digit,sum=0;
+scanf("%d",&s);
+             if(s%2==0)
+             {
+             	printf("%d",s);
+             }
+             else
+             {
+             	printf("%d",s-1);
+             }
+	
+return 0;
+}
