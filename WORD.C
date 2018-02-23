0461212bc3d68a88d85e80c300ab91bb92f0c69f
yamunasweety <35589149+yamunasweety@users.noreply.github.com>
2018-02-23 15:36:44 +0530

    WORD.C
    TO FIND THE WORD

---
+++ WORD.C
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main() 
+{
+int s;
+scanf("%d",&s);
+switch(s)
+{
+	case 1:
+	printf("\none");
+	break;
+	case 2:
+	printf("\ntwo");
+	break;
+	case 3:
+	printf("\nthree");
+	break;
+	case 4:
+	printf("\nfour");
+	break;
+	case 5: 
+	printf("\nfive");
+	break;
+	case 6:
+	printf("\nsix");
+	break;
+	case 7:
+	printf("\nseven");
+	break;
+	case 8:
+	printf("\neight");
+	break;
+	case 9:
+	printf("\nnine");
+	break;
+	default:
+printf("\nenter between 0 to 9");
+}
+return 0;
+}
