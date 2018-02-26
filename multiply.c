f6543e60411085648fe9e21f49dc74a6ff0a018d
yamunasweety <35589149+yamunasweety@users.noreply.github.com>
2018-02-26 16:19:08 +0530

    multiply.c
    to find the  multiply of numbers

---
+++ multiply.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int s;
+scanf("%d",&s);
+if(s%7==0)
+{
+printf("given no is multiple of 7");
+}
+else
+{
+printf("given no is not a multiple of 7");
+}
+return 0;
+}
