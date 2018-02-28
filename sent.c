d07835fc23c38edbf37b617f9c8589a61ea42748
yamunasweety <35589149+yamunasweety@users.noreply.github.com>
2018-02-28 16:14:55 +0530

    sent.c
    to find the perfect square

---
+++ sent.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int s,y,c,m;
+scanf("%d",&s);
+scanf("%d",&y);
+c=s*y;
+for(m=2;m<c;m++)
+{
+if(c==m*m);
+{
+printf("yes");
+break;
+}	
+ }
+  }	
