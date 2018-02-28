2da29215a0bf43552c59d31c886d4101074d1d2e
yamunasweety <35589149+yamunasweety@users.noreply.github.com>
2018-02-28 15:48:53 +0530

    composite.c
    to find the number of composite

---
+++ composite.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int num,s,flag=0;
+scanf("%d",&num);
+for(s=2;s<=num/2;s++)
+{
+if(num%s==0)
+{
+flag=1;
+break;
+}
+}
+if(flag==0)
+{
+printf("\n no");
+}
+else
+{
+printf("\n yes");
+}
+return 0;
+}
