c57d643808036d093d02ac2060aa208c9c17562e
yamunasweety <35589149+yamunasweety@users.noreply.github.com>
2018-03-05 16:29:47 +0530

    product.c
    to find the product

---
+++ product.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int num,product=1,digit;
+ printf("enter the number:");
+ scanf("%d",&num);
+ while(num!=0)
+ {
+  digit=num%10;
+  num=num/10;
+  product=product*digit;
+  }
+  printf("the product of digit is %d",product);
+  return 0;
+  }
