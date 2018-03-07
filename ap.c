1f0fca802fecd19b2a6fa0d77e74b2d7e0aa41e6
yamunasweety <35589149+yamunasweety@users.noreply.github.com>
2018-03-07 16:08:10 +0530

    ap.c
    to find the sum of alphabet values

---
+++ ap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<math.h>
+int main() 
+{
+int m,n,s,y;
+int M,N,S,Y;
+printf(" enter the number of terms:");
+scanf("%d",&m);
+printf(" enter the start value:");
+scanf("%d",&n);
+printf("enter the difference:");
+scanf("%d",&s);
+m=M-1;
+S=M*d;
+N=2*n;
+Y=N+S;
+y=Y*m/2;
+printf("%d",Y);
+return 0;
+}
